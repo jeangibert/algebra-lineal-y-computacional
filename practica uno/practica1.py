b76f70ebaa09a38e7a291f5e5bd608239e429874
Jean PC <jeanandregibert@gmail.com>
2026-03-29 18:40:39 -0300

    PC 29/3

--- practica uno/practica1.py
+++ practica uno/practica1.py
@@ -1,8 +1,25 @@
 import numpy as np;
-a= np.array([[1,1,-2,1],[3,-2,1,5],[1,-1,1,2]])
-b = np.array([-2,3,2])
-    
-x= np.linalg.solve(a,b)
+import matplotlib.pyplot as plt; 
+
+#### Ejercicio 4
+
+coef = np.array([[1,1,1],[4,2,1],[9,3,1]])
+coef_res = np.array([1,2,0])
+res = np.linalg.solve(coef,coef_res)
+
+print (res)
+
+xx = np.array([1,2,3])
+yy = np.array([1,2,0])
+
+x = np.linspace(0,4,100)
+f = lambda t:res[0]*t **2 + res[1]*t + res[2]
+plt.plot(xx,yy,'*')
+plt.plot(x,f(x))
+plt.show()
+
+
+
 
 
 
