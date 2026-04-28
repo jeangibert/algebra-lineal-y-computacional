1f7b7ce7dd3a8e71b362a3e3856a1c1f238f1d1d
facultad <facultad>
2026-04-28 16:47:12 -0300

    Ejercicio 2 hecho y comienzo logica ej3

--- practica tres/practica3.py
+++ practica tres/practica3.py
@@ -6,3 +6,17 @@ L = np.array([[1,0,0,0],[0,1,0,0],[2,1,1,0],[-3,0,0,1]])
 U = np.array([[1,-1,0,1],[0,1,4,0],[0,0,-4,-4],[0,0,0,2]])
 
 print((A == L @ U).all())
+
+#Ejercicio 3)
+#a)
+
+def solucionadorTriangularesInferiores(matriz,resultado):
+    """
+    Idea (a11,0,0      (y1)       (b1)
+          a21,a22,0    (y2)     = (b2)
+          a31,a32,a33) (y3)       (b3)
+
+    y1 = b1/a11
+    
+    
+    """
