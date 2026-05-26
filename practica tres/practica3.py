59091d8704ed9e1fafcf881fd73d00b3b2d7a3b5
Jean PC <jeanandregibert@gmail.com>
2026-05-26 19:55:23 -0300

    Practica 3 Ejercicio 3: Algoritmo + documentacion hecha para resolver matrices triangulares inferiores

--- practica tres/practica3.py
+++ practica tres/practica3.py
@@ -10,13 +10,30 @@ print((A == L @ U).all())
 #Ejercicio 3)
 #a)
 
-def solucionadorTriangularesInferiores(matriz,resultado):
-    """
-    Idea (a11,0,0      (y1)       (b1)
-          a21,a22,0    (y2)     = (b2)
-          a31,a32,a33) (y3)       (b3)
 
-    y1 = b1/a11
+import numpy as np
+
+"""
+La idea de este ejercicio es la siguiente;
+Debido a que es una matriz triangular lo que vamos a hacer es utilizar un i el cual vaya recorriendo las filas
+y que tambien sirva como indicador para toparse con las paredes de 0 ya que como L es triangular inferior
+la posicion de i concuerda con el ultimo elemento que no es 0 de la fila.
+Luego lo que usaremos  es un j el cual nos servira de acumulador y que recorrera los y desde la primera columna
+hasta la ultima y calcule el valor de ese y al despejarlo en la linea 21
+Vamos a ir guardando los valores de Y en el array y que lo llenamos con 0.
+Entonces iremos recorriendo cada fila con i , j calcularaa esa ecuacion de esa fila y lo guardaremos en el res
+"""
+def resolver_l_inferior(L:np.ndarray, b:np.ndarray):
+    dimensiones = L.shape
+    n = dimensiones[0]
+    y = np.zeros(n)
+    for i in range(n):
+        suma_acumulada = 0
+        for j in range(i):
+            suma_acumulada += (L[i][j] * y[j])
+        y[i]= (b[i] - suma_acumulada) / L[i][i]
+    return y    
     
-    
-    """
+# PD : el b de este inciso se hace solo adaptando el codigo ( los indices de los fors ) pero el algoritmo
+# es el mismo
+
